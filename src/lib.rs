44d99c304509a9981cbd0c4070541b20f3ccbc3a
GsLogiMaker <gslogimaker@gmail.com>
2022-05-11 10:58:36 -0500

    Added simple vm

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,22 @@
 
 //! TODO: Project level documentation.
 
-
 #![warn(missing_docs)]
 #![warn(rustdoc::missing_doc_code_examples)]
 #![warn(rustdoc::broken_intra_doc_links)]
 
-
 use std::fmt::{self};
 
+const OP_NULL:u8 = 0;
+const OP_CONST_TO_STACK:u8 = 1;
+const OP_ADD_U8:u8 = 2;
+const OP_JUMP_TO:u8 = 3;
+const OP_JUMP_BY:u8 = 4;
+const OP_JUMP_TO_IF:u8 = 5;
+const OP_JUMP_BY_IF:u8 = 6;
+
+const TK_VARIENT_NAME:u8 = 0;
+const TK_VARIENT_NUMBER:u8 = 1;
 
 /// An array of function pointers to be used in [`chars_fit_rule`].
 /// 
@@ -27,20 +35,22 @@ pub const RULES:&[& dyn Fn(&[char])->bool] = &[
 
 /// A slice of a script file with information on the row, column, and indent of
 /// the slice.
-pub struct Token<'a> {
+pub struct GeToken<'a> {
 	begin:u64,
 	end:u64,
 	row:u64,
 	_col:u64,
 	indent:u8,
 	source:&'a str,
+	varient:u8,
 
-} impl<'a> Token<'a> {
+} impl<'a> GeToken<'a> {
 
-	fn new(
+	/// Makes a new [`Token`].
+	pub fn new(
 			begin:u64, end:u64, row:u64, 
-			col:u64, indent:u8, source:&'a str) -> Token {
-		return Token{begin, end, row, _col: col, indent, source};
+			col:u64, indent:u8, source:&'a str) -> GeToken {
+		return GeToken{begin, end, row, _col: col, indent, source, varient:0};
 	}
 
 
@@ -58,7 +68,7 @@ pub struct Token<'a> {
 		return result;
 	}
 
-} impl<'a> std::fmt::Display for Token<'a> {
+} impl<'a> std::fmt::Display for GeToken<'a> {
 	
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		return write!(
@@ -67,6 +77,198 @@ pub struct Token<'a> {
 				self.row,
 				self.indent,);
 	}
+}
+
+
+pub struct GeVm {
+	registers:[i32;32],
+	pc:usize,
+	stack:Vec<u8>,
+	pub source:Vec<u8>,
+} impl GeVm {
+	
+	/// Makes a new [`Vm`].
+	pub fn new() -> Self {
+		return GeVm { pc:0, stack:vec![], source:vec![], registers:[0;32] };
+	}
+
+
+	fn u8_pair_to_u16(b1:u8, b2:u8) -> u16 {
+		return b1 as u16 + ((b2 as u16) << 8);
+	}
+
+
+	fn u16_pair_to_u32(b1:u16, b2:u16) -> u32 {
+		return b1 as u32 + ((b2 as u32) << 16);
+	}
+
+
+	fn u32_pair_to_u64(b1:u32, b2:u32) -> u64 {
+		return b1 as u64 + ((b2 as u64) << 32);
+	}
+
+
+	/// Compiles byte code from low level VM instructions.
+	pub fn compile_asm(code:&str) -> Vec<u8> {
+		let mut bcode = vec![];
+
+		let mut bytes_to_grab = 0;
+		for tk in tokenize(code) {
+			if bytes_to_grab > 0 {
+				bcode.push(
+						tk.text(tk.source)
+							.to_string()
+							.parse::<u8>()
+							.unwrap());
+				bytes_to_grab -= 1;
+				continue;
+			}
+
+			match tk.text(tk.source).to_lowercase().as_str() {
+				"end" => {
+					bcode.push(OP_NULL);
+				}
+				"const" => {
+					bcode.push(OP_CONST_TO_STACK);
+					bytes_to_grab = 2;
+				}
+				"add" => {
+					bcode.push(OP_ADD_U8);
+				}
+				_ => {
+					bcode.push(
+						tk.text(tk.source)
+							.to_string()
+							.parse::<u8>()
+							.unwrap());
+				}
+			}
+		}
+
+		return bcode;
+	}
+
+
+	/// Executes a load instruction.
+	fn exc_add_u8(&mut self) {
+		let rhs = self.stack.pop().expect("");
+		let lhs = self.stack.pop().expect("");
+		self.stack.push(lhs + rhs);
+	}
+
+
+	fn exc_jump_by(&mut self) {
+		let to_a = Self::u8_pair_to_u16(
+				self.stack.pop().expect(""),
+				self.stack.pop().expect("")
+				);
+		let to_b = Self::u8_pair_to_u16(
+				self.stack.pop().expect(""),
+				self.stack.pop().expect("")
+				);
+		self.pc += Self::u16_pair_to_u32(to_a, to_b) as usize;
+	}
+
+
+	fn exc_jump_by_if(&mut self) {
+		if self.next_u8() != 0 {
+			self.exc_jump_by();
+		} else {
+			self.stack.pop().expect("");
+			self.stack.pop().expect("");
+			self.stack.pop().expect("");
+			self.stack.pop().expect("");
+		}
+	}
+
+
+	fn exc_jump_to(&mut self) {
+		let to_a = Self::u8_pair_to_u16(
+				self.stack.pop().expect(""),
+				self.stack.pop().expect("")
+				);
+		let to_b = Self::u8_pair_to_u16(
+				self.stack.pop().expect(""),
+				self.stack.pop().expect("")
+				);
+		self.pc = Self::u16_pair_to_u32(to_a, to_b) as usize;
+	}
+
+
+	fn exc_jump_to_if(&mut self) {
+		if self.next_u8() != 0 {
+			self.exc_jump_to();
+		} else {
+			self.stack.pop().expect("");
+			self.stack.pop().expect("");
+			self.stack.pop().expect("");
+			self.stack.pop().expect("");
+		}
+	}
+
+
+	/// Executes a load instruction.
+	fn exc_load_const_u8(&mut self) {
+		let load_from = self.next_u16() as usize;
+		self.stack.push(self.source[load_from]);
+	}
+
+
+	/// Gets the next byte in the source code as a u8 int.
+	fn next_u8(&mut self) -> u8 {
+		let val = self.source[self.pc];
+		self.pc += 1;
+		return val;
+	}
+
+	/// Gets the next 2 bytes in the source code as a u16 int.
+	fn next_u16(&mut self) -> u16 {
+		return Self::u8_pair_to_u16(self.next_u8(), self.next_u8());
+	}
+
+
+	/// Gets the next 4 bytes in the source code as a u32 int.
+	fn next_u32(&mut self) -> u32 {
+		return Self::u16_pair_to_u32(
+				self.next_u16(), 
+				self.next_u16()
+				);
+	}
+
+	
+	pub fn run_bytes(&mut self, code:&[u8]) {
+		self.source = code.to_vec();
+		loop {
+			
+			match self.next_u8() {
+				OP_NULL => {
+					println!("Halting");
+					break;
+				}
+				OP_CONST_TO_STACK => {
+					self.exc_load_const_u8();
+				}
+				OP_ADD_U8 => {
+					self.exc_add_u8();
+				}
+				OP_JUMP_TO => {
+					self.exc_jump_to();
+				}
+				OP_JUMP_BY => {
+					self.exc_jump_by();
+				}
+				OP_JUMP_TO_IF => {
+					self.exc_jump_to_if();
+				}
+				OP_JUMP_BY_IF => {
+					self.exc_jump_by_if();
+				}
+				_ => { todo!() }
+			}
+
+		}
+	}
+
 }
 
 
@@ -228,7 +430,7 @@ pub fn tokenrule_symbols(added_so_far:&[char]) -> bool {
 ///	assert!(tokens[3].text(&script) == ";");
 ///	assert!(tokens[4].text(&script) == "!");
 /// ```
-pub fn tokenize<'a>(script:&'a str) -> Vec<Token> {
+pub fn tokenize<'a>(script:&'a str) -> Vec<GeToken> {
 	let mut tokens = vec!();
 
 	/* WARNING: This does not account for grapheme clusters. Currently hoping
@@ -269,7 +471,7 @@ pub fn tokenize<'a>(script:&'a str) -> Vec<Token> {
 		loop {
 			if chars_fit_rule(&added_so_far){
 				if curr_token <= tokens.len() && added_so_far.len() == 1 {
-					tokens.push(Token::new(
+					tokens.push(GeToken::new(
 						idx as u64,
 						idx as u64,
 						row,
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
 
 use ge_script::tokenize;
+use ge_script::GeVm;
 
+/// var i = 0
+/// var j = 2
+/// while i < 10
+/// 	j = j * j
+/// 	i = i + 1
+/// 
+/// stack push 0
+/// stack push 2
+/// 
+/// loop:
+/// 	# Multiply j = j * j
+/// 	push stack[-1] // Pushes value at -1 to top of stack
+///		push stack[-2] // Pushes value at -2 to top of stack
+/// 	mult // Muliplies the top two elements in stack
+/// 	move -2 // First highest element in stack to -2 in stack
+/// 
+/// 	# Add i = i + 1
+/// 	push stack[-2]
+/// 	push_val 1
+/// 	add
+/// 	move -3
+/// 
+/// # Less than i < 10
+/// push stack[-2]
+/// push_val 10
+/// lesser
+/// if_jump loop
 
 fn main() {
 	println!("");
@@ -13,8 +41,20 @@ var default = 128
 var dave.=5
 	";
 
-	let a = tokenize(script);
-	for token in a {
-		println!("{token}")
-	}
+	//let a = tokenize(script);
+	//for token in a {
+	//	println!("{token}")
+	//}
+
+	let asm = "
+		const 8 0
+		const 9 0
+		add
+		end
+		5
+		10";
+
+	let mut vm = GeVm::new();
+	vm.run_bytes( GeVm::compile_asm(asm).as_slice() );
+	println!("")
 }
